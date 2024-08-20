ca0a3a2d8daea225cd7b5e6989f9dc6472712d5d
Donato Paul <36795646+thedpaul@users.noreply.github.com>
2024-08-20 17:06:00 +0200

    Leveling (#6)
    * Implemented XP drops
    
    * Implemented XP drops
    
    * Implemented simple level scaling
    
    * Added simple HUD with XP progress bar
    
    * Fixed leveling and removed unnecessary nodes

--- Scripts/enemy_component.gd
+++ Scripts/enemy_component.gd
@@ -1,5 +1,9 @@
 extends Node2D
 
+@export var xp_scene : PackedScene
+
+@export_range(0, 100, 1, 'or_greater') var min_xp : int = 1
+@export_range(0, 100, 1, 'or_greater') var max_xp : int = 5
 
 func _ready():
 	for sibling in get_parent().get_children():
@@ -7,6 +11,12 @@ func _ready():
 			var healthcomp : HealthComponent = sibling
 			healthcomp.died.connect(_on_health_comp_died)
 
+func spawn_xp() -> void:
+	var xp : XP = xp_scene.instantiate()
+	xp.global_position = global_position
+	xp.value = randi_range(min_xp, max_xp)
+	get_tree().root.call_deferred('add_child', xp)
 
 func _on_health_comp_died():
-	StatTracker.enemy_killed.emit()
+	StatTracker.kill_enemy()
+	spawn_xp()
