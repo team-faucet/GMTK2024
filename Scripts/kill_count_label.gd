350a37c7c63b703ea2cc5e92292704326e72b345
thedpaul <dpaul2143@gmail.com>
2024-08-20 13:06:14 +0200

    Implemented simple level scaling

--- Scripts/kill_count_label.gd
+++ Scripts/kill_count_label.gd
@@ -4,7 +4,7 @@ extends Label
 func _ready():
 	StatTracker.enemy_killed.connect(_on_stat_update)
 	StatTracker.gained_xp.connect(_on_stat_update)
-	text = "Kills: 0\nXP: 0"
+	text = "Kills: 0\nXP: 0\nLvl: 0"
 
 func _on_stat_update():
-	text = "Kills: " + str(StatTracker.enemies_killed) + "\nXP: " + str(StatTracker.xp)
+	text = "Kills: " + str(StatTracker.enemies_killed) + "\nXP: " + str(StatTracker.xp) + "\nLvl: " + str(StatTracker.lvl)
--- Scripts/stat_tracker.gd
+++ Scripts/stat_tracker.gd
@@ -4,7 +4,9 @@ signal enemy_killed
 signal gained_xp
 
 var enemies_killed : int = 0
+
 var xp : int = 0
+var lvl : int = 1
 
 func kill_enemy():
 	enemies_killed += 1
@@ -12,4 +14,6 @@ func kill_enemy():
 
 func gain_xp(xp_in : int) -> void:
 	xp += xp_in
+	if (lvl >= 0):
+		lvl = floor(sqrt(xp))
 	gained_xp.emit()
