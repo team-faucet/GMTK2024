f773916486b0584c3dcf8225bd15eb73a16bad73
thedpaul <dpaul2143@gmail.com>
2024-08-20 19:38:30 +0200

    Added simple food/regeneration system

--- Scripts/enemy_component.gd
+++ Scripts/enemy_component.gd
@@ -1,6 +1,7 @@
 extends Node2D
 
 @export var xp_scene : PackedScene
+@export var food_scene : PackedScene
 
 @export_range(0, 100, 1, 'or_greater') var min_xp : int = 1
 @export_range(0, 100, 1, 'or_greater') var max_xp : int = 5
@@ -13,10 +14,17 @@ func _ready():
 
 func spawn_xp() -> void:
 	var xp : XP = xp_scene.instantiate()
-	xp.global_position = global_position
+	xp.global_position = global_position + 10 * Vector2(2. * randf() - 1., 2. * randf() - 1.)
 	xp.value = randi_range(min_xp, max_xp)
 	get_tree().root.call_deferred('add_child', xp)
+	
+func spawn_food() -> void:
+	var food : Food = food_scene.instantiate()
+	food.global_position = global_position + 10 * Vector2(2. * randf() - 1., 2. * randf() - 1.)
+	get_tree().root.call_deferred('add_child', food)
 
 func _on_health_comp_died():
 	StatTracker.kill_enemy()
 	spawn_xp()
+	if (randf() < 1/8.):
+		spawn_food()
