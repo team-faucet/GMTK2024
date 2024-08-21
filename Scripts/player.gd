9046ef3f36f3d627a5e274125d89481dc11535da
Donato Paul <36795646+thedpaul@users.noreply.github.com>
2024-08-21 21:16:42 +0200

    Obstacles (#14)
    * cleaned up project directory
    
    * Fixed crash due to bad tree structure
    - Enemies & projectiles were not spawned under the 'Main' node causing an illegal access on a previously freed 'Player' node on switching to the menu scene
    
    * Better referencing onto current scene
    
    * Added simple rock obstacle
    
    ---------
    
    Co-authored-by: fruechtekuchen <lfn2312@gmail.com>

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,16 +1,24 @@
-class_name Player extends Node2D
+class_name Player extends CharacterBody2D
 
 @export var speed = 400
-
 
 func _ready() -> void:
 	StatTracker.max_health = $HealthComponent.health
 	StatTracker.health = $HealthComponent.health
 
-func _process(delta):
-	var input := Input.get_vector("ui_left", "ui_right", "ui_up","ui_down")
-	position += input*speed*delta
-	rotation = -(get_viewport().get_mouse_position()-0.5*get_viewport_rect().size).angle_to(Vector2.UP)
+func _input(event: InputEvent) -> void:
+	if event is InputEventMouseMotion:
+		rotation = -(get_viewport().get_mouse_position()-0.5*get_viewport_rect().size).angle_to(Vector2.UP)
+	
+func _physics_process(delta: float) -> void:
+	var direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	if direction:
+		velocity = direction.normalized() * speed
+	else:
+		velocity.x = move_toward(velocity.x, 0, speed)
+		velocity.y = move_toward(velocity.y, 0, speed)
+	
+	move_and_slide()
 
 
 func _on_health_component_died():
