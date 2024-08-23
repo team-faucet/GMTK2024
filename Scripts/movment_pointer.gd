c4fb7ffef75416438ab70aadff28d8929ad92265
thedpaul <dpaul2143@gmail.com>
2024-08-23 10:26:12 +0200

    Fixed overspin of movement pointer

--- Scripts/movment_pointer.gd
+++ Scripts/movment_pointer.gd
@@ -3,5 +3,5 @@ extends Sprite2D
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	var tween: Tween = get_tree().create_tween()
-	tween.tween_property(self, "rotation", PI/2 + $"..".player.move_dir.angle_to(Vector2.UP), 0.1)
+	tween.tween_property(self, "rotation", lerp_angle(rotation, PI/2 + $"..".player.move_dir.angle_to(Vector2.UP), 1.), 0.1)
 	# rotation = PI/2 + $"..".player.move_dir.angle_to(Vector2.UP)
