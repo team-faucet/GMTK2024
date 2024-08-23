094fcecbfafac2c49727143de12e0fc154e29eef
Donato Paul <36795646+thedpaul@users.noreply.github.com>
2024-08-23 08:40:53 +0200

    Fixes & Better UI (#16)
    * Fixed corrupt UI
    
    * Improved UI
    
    * Added simple movment pointer to player's local HUD
    
    * Merged from 'origin/main'
    
    * Fixes and improved UI
    - Fixed bug: Drops persist on restart
    - Fixed bug: Enemy-to-enemy rigid body collision restored
    - Fixed bug: Set z-index of 'Food' sprite to 0
    - Removed unnecessary load statement
    - Adjustment and animation of movement pointer
    - Usage of CurveTextures for health bar instead Texture2Ds

--- Scripts/main_menu.gd
+++ Scripts/main_menu.gd
@@ -1,8 +1,7 @@
 extends Control
 
-func _on_play_button_button_up():
+func _on_play_button_button_up() -> void:
 	SceneManager.change_scene_to("res://main.tscn")
-	
 
-func _on_quit_button_button_up():
+func _on_quit_button_button_up() -> void:
 	get_tree().quit()
